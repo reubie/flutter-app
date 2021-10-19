0bd05c86bbd79babc015942db795fb92453e0e36
reubie <r.nguyo27@gmail.com>
2021-10-19 11:35:38 +0300

    creating a model class and a data class to populate listview of dictionary

---
+++ lib/data/dictionaries.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_app/model/dictionary.dart';
+
+final allDictionarydata = <Dictionary>[
+  Dictionary(number: '34', words: 'Thirty-four'),
+  Dictionary(number: '90', words: 'Ninety'),
+  Dictionary(number: '91', words: 'Ninety-one'),
+  Dictionary(number: '21', words: 'Twenty-one'),
+  Dictionary(number: '61', words: 'Sixty-one'),
+  Dictionary(number: '9', words: 'Nine'),
+  Dictionary(number: '2', words: 'Two'),
+  Dictionary(number: '6', words: 'Six'),
+  Dictionary(number: '3', words: 'Three'),
+  Dictionary(number: '8', words: 'Eight'),
+  Dictionary(number: '80', words: 'Eighty'),
+  Dictionary(number: '81', words: 'Eighty-one'),
+  Dictionary(number: '99', words: 'Ninety-Nine'),
+  Dictionary(number: '900', words: 'Nine-hundred'),
+];
